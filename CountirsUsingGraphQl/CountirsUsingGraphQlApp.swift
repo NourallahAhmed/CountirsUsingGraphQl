65fd010b8f892678114e2addbd81714543e410e5
NourallahAhmed <NourAllahahmed7001@gmail.com>
2023-01-24 03:26:34 -0800

    Initial Commit

---
+++ CountirsUsingGraphQl/CountirsUsingGraphQlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountirsUsingGraphQlApp.swift
+//  CountirsUsingGraphQl
+//
+//  Created by NourAllah Ahmed on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CountirsUsingGraphQlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
